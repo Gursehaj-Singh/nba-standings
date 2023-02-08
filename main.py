ba5fd7d8b6ba5ec868cb8c405fac0fa4e49b8a88
Gursehaj Singh <123921381+Gursehaj-Singh@users.noreply.github.com>
2023-02-07 21:53:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ soup = BeautifulSoup(page, "html.parser")
 teams = soup.find_all(class_='hide-mobile')
 wins = soup.find_all(class_='stat-cell')
 
-print(teams)
-print(wins)
-
 start = 0
 end = 13
 num = 1
 
-western_teams = {}
-for western_team in teams[0:15]:
-    western_teams[f"{num}. {western_team.text}"] = [x.text for x in wins[start:end]]
-    start += 13
-    end += 13
-    num += 1
-print(western_teams)
-
 eastern_teams = {}
-for eastern_team in teams[15:31]:
+for eastern_team in teams[0:15]:
     eastern_teams[f"{num}. {eastern_team.text}"] = [x.text for x in wins[start:end]]
     start += 13
     end += 13
     num += 1
+print("Eastern Conference")
 print(eastern_teams)
+
+num = 1
+western_teams = {}
+for western_team in teams[15:31]:
+    western_teams[f"{num}. {western_team.text}"] = [x.text for x in wins[start:end]]
+    start += 13
+    end += 13
+    num += 1
+
+print("Western Conference")
+print(western_teams)
